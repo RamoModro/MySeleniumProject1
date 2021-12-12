75298245d018def3ba66ee748e4c11a2e234808d
RamoModro <ramona.modrogan@gmail.com>
2021-12-12 14:53:30 +0200

    SeleniumProjectTemaLab16

---
+++ src/test/Java/WishListTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WishListTest {
+
+
+    public void addToWishlist(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.active.parent")).click();
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li > div > div.actions > ul > li:nth-child(1) > a")).click();
+        driver.close();
+    }
+
+
+}
